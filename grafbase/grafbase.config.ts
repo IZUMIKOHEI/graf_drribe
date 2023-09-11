2981aac63d378698f77c5071215e1eb89065bec0
liuyi <Code@Acer>
2023-09-11 22:18:18 +0800

    first commit reset

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -31,15 +31,6 @@ const Project = g
     rules.public().read(), rules.private().create().delete().update();
   });
 
-const jwt = auth.JWT({
-  issuer: "grafbase",
-  secret: g.env("NEXTAUTH_SECRET"),
-});
-
 export default config({
   schema: g,
-  auth: {
-    providers: [jwt],
-    rules: (rules) => rules.private(),
-  },
 });
